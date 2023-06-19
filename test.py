df5fc32e123a8d29d28e467cbaa986bbb946d46b
23BThomas <136824128+23BThomas@users.noreply.github.com>
2023-06-19 17:49:01 +0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+>>> like_cheese=True
+>>> like_pickles=False
+>>> like_cheese
+True
+>>> like_cheese and like_pickles
+False
+>>> like_cheese and not like_pickles
+True
+>>> like_cheese or like_pickles
+True
+>>> not like_cheese or like_pickles
+False
+>>> not like_cheese or not like_pickles
+True
